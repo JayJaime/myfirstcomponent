71b14e0a900cc1b0ae1c7b80fbad4583396ff42d
JayJaime <jj1998april@gmail.com>
2021-10-24 00:27:04 -0600

    change

--- src/components/main.js
+++ src/components/main.js
@@ -20,7 +20,7 @@ function main() {
             target="_blank"
             rel="noreferrer"
           >
-            JJ Linkedin
+            Linkedin
           </a>
         </div>
       </div>
